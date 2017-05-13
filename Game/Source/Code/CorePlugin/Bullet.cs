8d38fb9a9eae5c9b6ce5f8c306e809adc8229a4e
Adam <adam@adamslair.net>
2017-05-13 12:54:57 +0200

    Buller Collision Detection

--- Game/Source/Code/CorePlugin/Bullet.cs
+++ Game/Source/Code/CorePlugin/Bullet.cs
@@ -13,7 +13,28 @@ namespace Game
 {
 	[RequiredComponent(typeof(RigidBody))]
 	[RequiredComponent(typeof(SpriteRenderer))]
-	public class Bullet : Component
+	public class Bullet : Component, ICmpCollisionListener
 	{
+		void ICmpCollisionListener.OnCollisionBegin(Component sender, CollisionEventArgs args)
+		{
+			// Ignore collisions with other sensors
+			RigidBodyCollisionEventArgs bodyArgs = args as RigidBodyCollisionEventArgs;
+			if (bodyArgs.OtherShape.IsSensor) return;
+
+			// Ignore collisions with the player
+			bool isPlayer = args.CollideWith.GetComponent<PlayerShip>() != null;
+			if (isPlayer) return;
+
+			// Push the hit object a bit
+			RigidBody body = this.GameObj.GetComponent<RigidBody>();
+			Transform transform = this.GameObj.Transform;
+			RigidBody otherBody = args.CollideWith.GetComponent<RigidBody>();
+			otherBody.ApplyWorldImpulse(body.LinearVelocity * 2.0f, transform.Pos.Xy);
+
+			// Get rid of the bullet
+			this.GameObj.DisposeLater();
+		}
+		void ICmpCollisionListener.OnCollisionEnd(Component sender, CollisionEventArgs args) { }
+		void ICmpCollisionListener.OnCollisionSolve(Component sender, CollisionEventArgs args) { }
 	}
 }
